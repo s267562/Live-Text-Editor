fd2ddd468279b26984f1d6612b5c211c8ee080dc
Simone <s267562@studenti.polito.it>
2019-07-20 02:30:14 +0200

    text editor
    first version

---
+++ texteditor/cmake-build-debug/texteditor_autogen/EWIEGA46WW/moc_textedit.cpp
@@ -0,0 +1,160 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'textedit.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../textedit.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'textedit.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_TextEdit_t {
+    QByteArrayData data[14];
+    char stringdata0[126];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_TextEdit_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_TextEdit_t qt_meta_stringdata_TextEdit = {
+    {
+QT_MOC_LITERAL(0, 0, 8), // "TextEdit"
+QT_MOC_LITERAL(1, 9, 7), // "fileNew"
+QT_MOC_LITERAL(2, 17, 0), // ""
+QT_MOC_LITERAL(3, 18, 8), // "fileOpen"
+QT_MOC_LITERAL(4, 27, 8), // "fileSave"
+QT_MOC_LITERAL(5, 36, 10), // "fileSaveAs"
+QT_MOC_LITERAL(6, 47, 13), // "fileExportPdf"
+QT_MOC_LITERAL(7, 61, 8), // "textBold"
+QT_MOC_LITERAL(8, 70, 13), // "textUnderline"
+QT_MOC_LITERAL(9, 84, 10), // "textItalic"
+QT_MOC_LITERAL(10, 95, 9), // "textAlign"
+QT_MOC_LITERAL(11, 105, 8), // "QAction*"
+QT_MOC_LITERAL(12, 114, 1), // "a"
+QT_MOC_LITERAL(13, 116, 9) // "textColor"
+
+    },
+    "TextEdit\0fileNew\0\0fileOpen\0fileSave\0"
+    "fileSaveAs\0fileExportPdf\0textBold\0"
+    "textUnderline\0textItalic\0textAlign\0"
+    "QAction*\0a\0textColor"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_TextEdit[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      10,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   64,    2, 0x08 /* Private */,
+       3,    0,   65,    2, 0x08 /* Private */,
+       4,    0,   66,    2, 0x08 /* Private */,
+       5,    0,   67,    2, 0x08 /* Private */,
+       6,    0,   68,    2, 0x08 /* Private */,
+       7,    0,   69,    2, 0x08 /* Private */,
+       8,    0,   70,    2, 0x08 /* Private */,
+       9,    0,   71,    2, 0x08 /* Private */,
+      10,    1,   72,    2, 0x08 /* Private */,
+      13,    0,   75,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Bool,
+    QMetaType::Bool,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 11,   12,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void TextEdit::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<TextEdit *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->fileNew(); break;
+        case 1: _t->fileOpen(); break;
+        case 2: { bool _r = _t->fileSave();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 3: { bool _r = _t->fileSaveAs();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 4: _t->fileExportPdf(); break;
+        case 5: _t->textBold(); break;
+        case 6: _t->textUnderline(); break;
+        case 7: _t->textItalic(); break;
+        case 8: _t->textAlign((*reinterpret_cast< QAction*(*)>(_a[1]))); break;
+        case 9: _t->textColor(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject TextEdit::staticMetaObject = { {
+    &QMainWindow::staticMetaObject,
+    qt_meta_stringdata_TextEdit.data,
+    qt_meta_data_TextEdit,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *TextEdit::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *TextEdit::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_TextEdit.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int TextEdit::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 10)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 10;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 10)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 10;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
