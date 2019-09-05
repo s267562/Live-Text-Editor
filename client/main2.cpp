3f3aa3e7ed129abd71796114790131861c166793
Simone <s267562@studenti.polito.it>
2019-09-05 23:43:55 +0200

    fix second client

--- client/main2.cpp
+++ client/main2.cpp
@@ -13,47 +13,52 @@ int main(int argc, char *argv[]) {
 #if UI
     Controller controller;
 #else
-#if REGISTRATION_TEST
-    /* TEST: REGISTRATION FEATURE */
+    #if REGISTRATION_TEST
+        /* TEST: REGISTRATION FEATURE */
         Messanger messanger;
         messanger.connectTo("127.0.0.1");
         messanger.registration("ciao","ciao","/Users/andrea/Documents/sfondi/preview.jpeg");
-#else
-    /* TEST: TEXT EDITOR */
-    Messanger messanger;
+    #else
+        /* TEST: TEXT EDITOR */
+        Messanger messanger;
 
-    CRDT crdt{}; // model
-    messanger.setCRDT(&crdt);
+        CRDT crdt{}; // model
+        messanger.setCRDT( &crdt);
 
-    messanger.connectTo("127.0.0.1");
-    QString username = "u2";
-    messanger.logIn(username, "prova");
+        messanger.connectTo("127.0.0.1");
+        QString username = "u2";
+        messanger.logIn(username, "prova");
 
-    QString siteId = username;
-    crdt.setSiteId(siteId);
+        QString siteId = username;
+        crdt.setSiteId(siteId);
 
-    Q_INIT_RESOURCE(textEditor);
+        Q_INIT_RESOURCE(textEditor);
 
-    // The QApplication class manages the GUI application's control flow and main settings.
-    QCoreApplication::setOrganizationName("QtProject");
-    QCoreApplication::setApplicationName("text editor");
-    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+        // The QApplication class manages the GUI application's control flow and main settings.
+        QCoreApplication::setOrganizationName("QtProject");
+        QCoreApplication::setApplicationName("text editor");
+        QCoreApplication::setApplicationVersion(QT_VERSION_STR);
 
-    QCommandLineParser parser;
-    parser.setApplicationDescription(QCoreApplication::applicationName());
-    parser.addHelpOption();
-    parser.addVersionOption();
-    parser.addPositionalArgument("file", "The file to open.");
-    parser.process(app);
+        QCommandLineParser parser;
+        parser.setApplicationDescription(QCoreApplication::applicationName());
+        parser.addHelpOption();
+        parser.addVersionOption();
+        parser.addPositionalArgument("file", "The file to open.");
+        parser.process(app);
 
-    // view
-    Editor editor{siteId};
+        // view
+        Editor editor{siteId};
 
-    // controller
-    Controller controller{&crdt, &editor, &messanger};
+        // controller
+        Controller controller{&crdt, &editor, &messanger};
 
-    editor.show();
-#endif
+        /*const QRect availableGeometry = QApplication::desktop()->availableGeometry(&editor);
+        editor.resize(availableGeometry.width() / 2, (availableGeometry.height() * 2) / 3);
+        editor.move((availableGeometry.width() - editor.width()) / 2,
+                (availableGeometry.height() - editor.height()) / 2);*/
+
+        editor.show();
+    #endif
 #endif
     return app.exec();
 }
