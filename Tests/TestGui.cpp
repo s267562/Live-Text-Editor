b8776aafee4d4d9a78837f7e19e8d7f1cf11c6a7
eugenio <s265495@studenti.polito.it>
2020-03-02 11:28:39 +0100

    Add TestCaseGui

--- Tests/TestGui.cpp
+++ Tests/TestGui.cpp
@@ -12,6 +12,8 @@
 #include <QtCore/qnamespace.h>
 #include "../client/Controller.h"
 
+
+
 class TestGui: public QObject
 {
 
@@ -22,9 +24,17 @@ private slots:
     void testGui();
 
 
+
 private:
     Controller controller;
+    Controller controller2;
+
+    void logClient(Controller &controller, QString username, QString password);
+    void generalAction(Controller &controller);
+
+
 };
+
 
 //! [1]
 void TestGui::testGui()
@@ -32,50 +42,51 @@ void TestGui::testGui()
 
     QApplication::setActiveWindow(&controller);
 
+    QApplication::setActiveWindow(&controller2);
+
+    logClient(controller, "u1","u1");
+
+    logClient(controller2, "u2","u2");
+
+    // generalAction(controller);
+
+    QTest::qWait(15000);
+
+
+
+
+
+}
+//! [1]
+
+
+void TestGui::logClient(Controller &controller, QString username, QString password){
+
     QTest::qWaitForWindowExposed(controller.findChild<QMainWindow*>("Connection"));
 
-    //QTest::keyClicks(controller.findChild<QLineEdit*>("serverIP"),"127.0.0.1");
-    //QTest::keyClicks(controller.findChild<QLineEdit*>("serverPort"),"1234");
     QTest::mouseClick(controller.findChild<QMainWindow*>("Connection")->findChild<QPushButton*>("pushButton"),Qt::LeftButton);
 
     QTest::qWaitForWindowExposed(controller.findChild<QMainWindow*>("Login"));
 
-    QTest::keyClicks(controller.findChild<QMainWindow*>("Login")->findChild<QLineEdit*>("username"),"u1");
-    QTest::keyClicks(controller.findChild<QMainWindow*>("Login")->findChild<QLineEdit*>("password"),"u1");
+    QTest::keyClicks(controller.findChild<QMainWindow*>("Login")->findChild<QLineEdit*>("username"),username);
+    QTest::keyClicks(controller.findChild<QMainWindow*>("Login")->findChild<QLineEdit*>("password"),password);
     QTest::mouseClick(controller.findChild<QMainWindow*>("Login")->findChild<QPushButton*>("pushButton"),Qt::LeftButton);
 
     QTest::qWaitForWindowExposed(controller.findChild<QMainWindow*>("ShowFiles"));
 
-    //QTest::mouseDClick(controller.findChild<QMainWindow*>("ShowFiles")->findChild<QListWidget*>("listWidget")->findChild<QListWidgetItem*>("file2"),Qt::LeftButton);
-
 
     QListWidgetItem* i = controller.findChild<QMainWindow*>("ShowFiles")->findChild<QListWidget*>("listWidget")->item(1);
     controller.findChild<QMainWindow*>("ShowFiles")->findChild<QListWidget*>("listWidget")->itemDoubleClicked(i);
-    //controller.findChild<QListWidget*>("listWidget")->itemDoubleClicked(file);
 
     QTest::qWaitForWindowExposed(controller.findChild<QMainWindow*>("Editor"));
+}
 
-//    for(auto c:controller.findChild<QMainWindow*>("Editor")->findChild<QToolBar*>("")->children()){
-//
-//        qDebug() << c;
-//
-//
-//
-//    }
-//
-//    qDebug() << controller.findChild<QMainWindow*>("Editor")->findChild<QToolBar*>("")->findChildren<QToolButton*>("",Qt::FindDirectChildrenOnly).at(4)->text();
-//
-//    for(auto c:controller.findChild<QMainWindow*>("Editor")->findChild<QActionGroup*>("")->actions()){
-//
-//        qDebug() << c;
-//
-//    }
-
+void TestGui::generalAction(Controller &controller) {
     QTest::mouseClick(controller.findChild<QMainWindow*>("Editor")->findChild<QToolBar*>("")->findChildren<QToolButton*>("",Qt::FindDirectChildrenOnly).at(4), Qt::LeftButton);
-    
+
     QTest::keyClicks(controller.findChild<QMainWindow*>("Editor")->centralWidget(), "Ciaovfsnfvsotnveht gveaovghqeovhgaergv", Qt::NoModifier, 20);
     QTest::keyClick(controller.findChild<QMainWindow*>("Editor")->centralWidget(), Qt::Key_Enter, Qt::NoModifier, 100);
-    
+
     QTest::keyClicks(controller.findChild<QMainWindow*>("Editor")->centralWidget(), "Ciaovchreicaibvasdvhqeorgv", Qt::NoModifier, 20);
     QTest::keyClick(controller.findChild<QMainWindow*>("Editor")->centralWidget(), Qt::Key_Enter, Qt::NoModifier, 100);
     QTest::keyClick(controller.findChild<QMainWindow*>("Editor")->centralWidget(), Qt::Key_Enter, Qt::NoModifier, 100);
@@ -111,7 +122,7 @@ void TestGui::testGui()
 
     QTest::keyClick(controller.findChild<QMainWindow*>("Editor")->centralWidget(), Qt::Key_Up, Qt::NoModifier, 300);
     QTest::mouseClick(controller.findChild<QMainWindow*>("Editor")->findChild<QToolBar*>("")->findChildren<QToolButton*>("",Qt::FindDirectChildrenOnly).at(4), Qt::LeftButton);
-    
+
     QTest::keyClick(controller.findChild<QMainWindow*>("Editor")->centralWidget(), Qt::Key_Up, Qt::NoModifier, 300);
     QTest::mouseClick(controller.findChild<QMainWindow*>("Editor")->findChild<QToolBar*>("")->findChildren<QToolButton*>("",Qt::FindDirectChildrenOnly).at(4), Qt::LeftButton);
 
@@ -144,48 +155,7 @@ void TestGui::testGui()
 
 
     QTest::mouseClick(controller.findChild<QMainWindow*>("Editor")->findChild<QToolBar*>("")->findChildren<QToolButton*>("",Qt::FindDirectChildrenOnly).at(3), Qt::LeftButton);
-
-
-    QTest::qWait(15000);
-
-    //controller.s
-
-    //controller.requestForFile("file2");
-
-
-//    Messanger messanger;
-//
-//    CRDT crdt{}; // model
-//    messanger.setCRDT( &crdt);
-//
-//    messanger.connectTo("127.0.0.1", "1234");
-//    QString username = "u1";
-//    messanger.logIn(username, "u1");                         /* NOTA: occorre registrare l'utente!! */
-//
-//    messanger.requestForFile("file2");
-//
-//    QString siteId = username;
-//    crdt.setSiteId(siteId);
-//
-//    Q_INIT_RESOURCE(textEditor);
-//
-//    // view
-//    Editor editor{siteId};
-//
-//    // controller
-//    Controller controller{&crdt, &editor, &messanger};
-//
-//    editor.setController(&controller);
-
-    /*const QRect availableGeometry = QApplication::desktop()->availableGeometry(&editor);
-    editor.resize(availableGeometry.width() / 2, (availableGeometry.height() * 2) / 3);
-    editor.move((availableGeometry.width() - editor.width()) / 2,
-            (availableGeometry.height() - editor.height()) / 2);*/
-
-
-
 }
-//! [1]
 
 //! [2]
 QTEST_MAIN(TestGui)
