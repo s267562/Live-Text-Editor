5f636d97b8c0eb9c0a19a1c17d4a79bc17717b1d
Davide Sordi <s267570@studenti.polito.it>
2020-05-16 16:31:43 +0200

    Save backup function from old branch

---
+++ server/Utils/Utilities.cpp
@@ -0,0 +1,13 @@
+/**
+ * Created by Davide Sordi
+ * Using CLion
+ * Date: 16/05/2020
+ * Time: 16.08
+ * 
+ * Class: Utilities
+ * File: Utilities.cpp	
+ * Project: textEditor	
+ */
+
+
+#include "Utilities.h"
---
+++ server/Utils/Utilities.h
@@ -0,0 +1,18 @@
+/**
+ * Created by Davide Sordi
+ * Using CLion
+ * Date: 16/05/2020
+ * Time: 16.08
+ * 
+ * Class: Utilities
+ * File: Utilities.h	
+ * Project: textEditor	
+ */
+
+
+#include "Utilities.h"
+
+#ifndef TEXTEDITOR_UTILITIES_H
+#define TEXTEDITOR_UTILITIES_H
+
+#endif //TEXTEDITOR_UTILITIES_H
