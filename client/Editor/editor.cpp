93d76c42c0b9025d7571fabdf9ae80266fb1418d
marinele <eugenio.marinelli@eurecom.fr>
2020-07-26 15:49:19 +0200

    Revert "Bug fix: selection including last char"
    This reverts commit 104c8613

--- client/Editor/editor.cpp
+++ client/Editor/editor.cpp
@@ -414,7 +414,7 @@ void Editor::onTextChanged(int position, int charsRemoved, int charsAdded) {
 				isWorkingLock.unlock();
 
 				if (cursorPos != startSelection) { // Selection forward
-                    if(position == 0 && textDocument->characterCount() - 1 != charsAdded){
+                    if(position==0){
                         charsAdded++;
                     }
 					for (int i = 0; i < charsAdded; i++) {
@@ -432,7 +432,7 @@ void Editor::onTextChanged(int position, int charsRemoved, int charsAdded) {
 												  Q_ARG(QTextCharFormat, textCharFormat), Q_ARG(Pos, pos));
 					}
 				} else { // Selection backward
-                    if(position == 0 && textDocument->characterCount() - 1 != charsAdded){
+                    if(position==0){
                         charsAdded++;
                     }
 					for (int i = charsAdded-1; i >= 0; i--) {
